847d71c9668c5968dcbdb28421e56f7393c87357
patrickofriel-wk <patrick.ofriel@workiva.com>
2019-03-15 15:42:44 -0400

    Add additional test

--- test/select.spec.js
+++ test/select.spec.js
@@ -441,6 +441,26 @@ describe('select', () => {
           }
         ]);
       });
+
+      it('should parse try_casted varchar columns', () => {
+        ast = parser.parse('select try_cast(t.c as varchar) as dc from t');
+        expect(ast.columns).to.eql([
+          { 
+            expr: { 
+              type: 'try_cast', 
+              target: {
+                dataType: 'VARCHAR'
+              }, 
+              expr: { 
+                type: 'column_ref', 
+                table: 't', 
+                column: 'c' 
+              }
+            },  
+            as: 'dc' 
+          }
+        ]);
+      });
     });
 
     describe('from clause', () => {
